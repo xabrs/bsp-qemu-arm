3ccae1eb461324d314d932898feb5aba3bf99be4
xabrs <igavril0000@gmail.com>
2021-12-11 01:49:35 +0900

    Update start_qemu_win.bat

--- start_qemu_win.bat
+++ start_qemu_win.bat
@@ -1,7 +1,7 @@
 rem U-Boot commands:
-rem > virtio scan
-rem > fatload virtio 0:1 0x40200000 bsp-qemu-virt.bin
-rem > go 0x40200000
+rem virtio scan
+rem fatload virtio 0:1 0x40200000 bsp-qemu-virt.bin
+rem go 0x40200000
 C:\Program Files (x86)\qemu\qemu-system-arm.exe -M virt -m 256 -cpu cortex-a15 -bios u-boot.bin -hda fat:rw:./images -vga none -serial stdio
 
 rem For redirect serial to TCP:
